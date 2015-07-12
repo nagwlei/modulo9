6c70b277b2ba1c9eec0e5f0b640aa23ba604a840
Leire Roa <leireroa@gmail.com>
2015-07-12 15:27:22 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',				quizController.index);
+router.get('/quizes/:quizId(\\d+)',		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
 router.get('/author', function(req, res) {
     res.render('author', { authors: [{ name: 'Leire Roa', urlphoto: '/images/author.jpg', urlvideo: 'videos/30-SecondVideo.mp4' }, { name: 'Usuario Prueba', urlphoto: '/images/usuario.jpeg' }]});

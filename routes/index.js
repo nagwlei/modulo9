3a1e50be15c466fcc1f9946779f5723d394a1108
Leire Roa <leireroa@gmail.com>
2015-07-14 11:07:42 +0200

    Validación de entradas

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -18,8 +18,10 @@ router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 router.get('/quizes/new',			quizController.new);
 router.post('/quizes/create',			quizController.create);
 
-router.get('/author', function(req, res) {
-    res.render('author', { authors: [{ name: 'Leire Roa', urlphoto: '/images/author.jpg', urlvideo: 'videos/30-SecondVideo.mp4' }, { name: 'Usuario Prueba', urlphoto: '/images/usuario.jpeg' }]});
-});
+/*router.get('/author', function(req, res) {
+    res.render('author', { authors: [{ name: 'Leire Roa', urlphoto: '/images/author.jpg', urlvideo: 'videos/30-SecondVideo.mp4' }, { name: 'Usuario Prueba', urlphoto: '/images/usuario.jpeg' }]} );
+});*/
+//PRUEBA CON CONTROLADOR
+router.get('/author',				quizController.author);
 
 module.exports = router;
